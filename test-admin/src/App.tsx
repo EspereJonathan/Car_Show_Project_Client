265d5448551e46eae6a5fcbb46141945366c6d21
EspereJonathan <hei.espere@gmail.com>
2024-06-07 19:36:32 +0300

    feat: add login, signup, dashboard

--- test-admin/src/App.tsx
+++ test-admin/src/App.tsx
@@ -1,9 +1,10 @@
-import {
-  Admin,
-  Resource,
-  ListGuesser,
-  EditGuesser,
-  ShowGuesser,
-} from "react-admin";
+import { Admin, Resource } from "react-admin";
+import { UserList } from "./users";
+import { Dashboard } from './Dashboard';
 
-export const App = () => <Admin></Admin>;
+
+export const App = () => (
+  <Admin dashboard={Dashboard}>
+     <Resource name="users" list={UserList} />
+  </Admin>
+);
